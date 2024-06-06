06b2db09a01cac07d81c8ad35d0916ff33e6e52f
muzaffarbhat07 <bhatmuzaffar32@gmail.com>
2024-06-06 17:26:00 +0530

    dummy: add dummy projects page

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,7 +1,9 @@
 
 const Project = () => {
   return (
-    <div className="flex flex-wrap items-center justify-center flex-col lg:flex-row relative overflow-hidden h-screen"></div>
+    <div className="flex flex-wrap items-center justify-center flex-col lg:flex-row relative overflow-hidden h-screen">
+      <span className="text-white text-xl">UPDATING SOON...</span>
+    </div>
   )
 }
 
