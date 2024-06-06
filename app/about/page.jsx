97066b9ffe6bad8c1f60190070a1976da5f6207f
muzaffarbhat07 <bhatmuzaffar32@gmail.com>
2024-06-06 14:19:16 +0530

    style: tools  section design

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,5 +1,6 @@
 import Github from "@/components/Github"
 import Techstack from "@/components/Techstack"
+import Toolstack from "@/components/Toolstack"
 import WhoIM from "@/components/WhoIM"
 
 const About = () => {
@@ -10,6 +11,10 @@ const About = () => {
         Professional <p className="text-[#ad50eb]">Skillset </p>
       </h1>
       <Techstack />
+      <h1 className="m-auto flex items-center w-full justify-center gap-2 text-gray-200 font-medium text-4xl my-3">
+        <p className="text-[#ad50eb]">Tools </p> I Use
+      </h1>
+      <Toolstack />
       <Github />
     </div>
   )
