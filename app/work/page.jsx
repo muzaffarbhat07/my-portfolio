d1fe0308f88e94c4584bc781ebd937dcbb6d0786
muzaffarbhat07 <bhatmuzaffar32@gmail.com>
2024-06-07 20:01:30 +0530

    update: added projects

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,8 +1,9 @@
+import Projects from "@/components/Projects"
 
 const Work = () => {
   return (
-    <div className="flex flex-wrap items-center justify-center flex-col lg:flex-row relative overflow-hidden h-screen">
-      <span className="text-white text-xl">UPDATING SOON...</span>
+    <div className="flex flex-wrap items-center justify-center flex-col lg:flex-row relative overflow-hidden ">
+      <Projects />
     </div>
   )
 }
---
+++ components/Projects.jsx
@@ -0,0 +1,42 @@
+import ProjectCard from "@/components/ProjectCard"
+
+const projectsData = [
+  {
+    name: "Online Code Editor",
+    image: "/assets/images/projects/editor.jpg",
+    about: "It's an Online Code-Editor for HTML, CSS, JS code snippets with the functionality similar to that of codepen.io. To save a user's work, localStorage is used.",
+    demo: "https://muzaffarbhat07.github.io/online-code-editor/",
+    code: "https://github.com/muzaffarbhat07/online-code-editor",
+  },
+  {
+    name: "Muzaffar's Blog",
+    image: "/assets/images/projects/blog.jpg",
+    about: "It's my personal blog app where I share blogs about tech. Users can read, comment and like the blogs.",
+    demo: "https://muzaffars-blog.onrender.com/",
+    code: "https://github.com/muzaffarbhat07/muzaffars-blog",
+  },
+  {
+    name: "Promptopia",
+    image: "/assets/images/projects/promptopia.jpg",
+    about: "Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts.",
+    demo: "https://promptopia-muzaffar.vercel.app/",
+    code: "https://github.com/muzaffarbhat07/promptopia",
+  },
+]
+
+const Projects = () => {
+  return (
+    <>
+      <img
+        src="/assets/images/star.jpg"
+        alt=""
+        className="h-full w-full object-cover opacity-20 absolute bottom-0"
+      />
+      <div className="z-20 flex items-center justify-center gap-3 m-2 flex-wrap">
+        {projectsData.map((project, i) => <ProjectCard project={project} key={i} />)}
+      </div>
+    </>
+  )
+}
+
+export default Projects;
